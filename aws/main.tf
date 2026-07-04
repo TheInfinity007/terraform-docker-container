3432dec04787ef7d0ca5d58b13e6b266c8f18599
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2026-07-05 03:30:16 +0530

    Interpolate variable in string

--- aws/main.tf
+++ aws/main.tf
@@ -26,7 +26,7 @@ module "app_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/web"
   version = "4.17.0"
 
-  name        = "web-sg-project-alpha-dev"
+  name        = "web-sg-${var.resource_tags["project"]}-${var.resource_tags["environment"]}"
   description = "Security group for web servers with HTTP ports open within VPC"
   vpc_id      = module.vpc.vpc_id
 
@@ -39,7 +39,7 @@ module "lb_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/web"
   version = "4.17.0"
 
-  name        = "lb-sg-project-alpha-dev"
+  name        = "lb-sg-${var.resource_tags["project"]}-${var.resource_tags["environment"]}"
   description = "Security group for load balancer with HTTP ports open within VPC"
   vpc_id      = module.vpc.vpc_id
 
